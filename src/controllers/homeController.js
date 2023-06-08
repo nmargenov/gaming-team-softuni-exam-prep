da9a1cea487af60d09fc813b2eed07f54e708297
nmargenov <nikolay_margenov@yahoo.com>
2023-06-08 22:21:06 +0300

    search works

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,36 @@
+const { getAllGames } = require('../managers/gameManager');
+const { mustBeAuth } = require('../middlewares/authMiddleware');
+const { optionsGenerator } = require('../utils/utils');
+
 const router = require('express').Router();
 
 router.get(['/','/index'],(req,res)=>{
     res.status(302).render('home');
 });
 
-router.get('/search',(req,res)=>{
-    res.status(302).render('search');
+router.get('/search',mustBeAuth,async (req,res)=>{
+    try{
+        let games = await getAllGames().lean();
+        const hasGames = games.length>0;
+        
+        const search = req.query.search;
+        const platform = req.query.platform;
+        
+        if(search){
+            games = games.filter(game=>game.name.toLowerCase().includes(search.trim()));
+        }
+
+        if(platform){
+            games = games.filter(game=>game.platform==platform);
+        }
+
+        const options = optionsGenerator(platform,true);
+
+        res.status(302).render('search',{games,hasGames,search,options});
+    }catch(err){
+        console.log(err);
+        res.status(404).render('404');
+    }
 })
 
 module.exports = router;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,10 @@
-exports.optionsGenerator = (selected)=>{
+exports.optionsGenerator = (selected,neutral=false)=>{
     const platforms = ["PC","Nintendo","PS4","PS5","XBOX"];
 
     let html = "";
+    if(neutral){
+        html +=`<option value="">-------</option>`;
+    }
     platforms.forEach(platform => {
         html+=`<option value="${platform}" ${selected == platform ? 'selected' : ''}>${platform}</option>`;
     });
