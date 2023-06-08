2bcf80d3523d2aec8ac063b4f043e562bd5ea7cb
nmargenov <nikolay_margenov@yahoo.com>
2023-06-08 18:14:53 +0300

    Add initial config files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
+const connectDatabase = require('./config/databaseConfig');
 const { PORT } = require('./config/config');
 
 const app = express();
+
+expressConfig(app);
+handlebarsConfig(app);
+
+connectDatabase()
+    .then(()=>console.log('Database is successfully connected!'))
+    .catch(err =>{console.log('Error connecting to the database: ',err)});
 
 app.get(['/','/index'],(req,res)=>{
     res.send("Server is working!");
