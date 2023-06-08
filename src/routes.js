fdfaf66b3ba2165074daab4616b4e71e1c3b3e12
nmargenov <nikolay_margenov@yahoo.com>
2023-06-08 18:26:39 +0300

    fix navigation links

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 
-router.use(homeController)
+router.use(homeController);
+
+router.get('*',(req,res)=>{
+    res.status(404).render('404');
+})
 
 module.exports = router;    
